660ff6012a4a863b75f35c4f8e694af5dc46b0d0
Pedro Alejandro Mosquera <alejandro.mosquera@dynatrace.com>
2020-04-25 15:35:41 +0200

    Adding docs

--- next.config.js
+++ next.config.js
@@ -1,2 +1,15 @@
 const withSass = require('@zeit/next-sass');
-module.exports = withSass({});
+const withOffline = require('next-offline')
+
+module.exports = withOffline(withSass({
+    webpack: (config, { isServer }) => {
+        // Fixes npm packages that depend on `fs` module
+        if (!isServer) {
+          config.node = {
+            fs: 'empty'
+          }
+        }
+
+        return config
+      }
+}));
