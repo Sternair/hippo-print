11bd484020b69af05efeb92a0fccb6682f703b89
Pedro Alejandro Mosquera <alejandro.mosquera@dynatrace.com>
2020-04-25 15:29:24 +0200

    Adding next.js basis

--- pages/App.tsx
+++ pages/App.tsx
@@ -5,14 +5,14 @@ import "./style.scss";
 function App() {
   return (
    <>
-  
+
 <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
   <div className="container">
     <a className="navbar-brand" href="#">
           <img src="hippo_print.jpg" alt=""></img>
         </a>
     <a className="navbar-brand" href="#">Hippo Print</a>
-    
+
     <ul className="navbar-nav ml-auto">
     <li className="nav-item">
       <a className="nav-link" href="#">Covid-19</a>
@@ -33,18 +33,18 @@ function App() {
     <li><button type="submit" className="btn navbar-btn btn-info" name="sign-up" id="sign-up"  value="Sign up">Sign Up</button></li>
   </ul>
   </div>
-</nav>          
+</nav>
 
-            
+
 <div className="container">
     <div className="row">
         <div className="col-6" id="assistance">
             <h2>Built for Medical Innovators</h2>
-    
+
             <h1>Europe’s Digital Manufacturing Community Making Medical Manufacturing Local Again</h1>
-    
+
             <p>HIPPO PRINT is a collaborative community in digital manufacturing. We accelerate the adoption of open source digital manufacturing technologies that focus on optimizing the medical supply chain and increase efficiency and agility, while lowering the carbon footprint and price.</p>
-       
+
     <div className="signup form " id="signupform">
 		   <form action="" method="post" name="login">
 			  <div className="form-group">
@@ -74,67 +74,67 @@ function App() {
 			  </p>
 		   </form>
 		</div>
-       
-       
-       
+
+
+
         </div>
-    
-        
+
+
         <div className="col-6">
-            
+
                 <div className="col-12-sm" id ="idea"> <div className="idea1">Health Providers</div>
                 <div className="idea2">Get your project evaluated, and get an overview of your funding opportunities</div>
                 </div>
-    
+
                 <div className="col-12-sm" id="database"><div className="data1"> COVID-19 Collection</div>
                 <div className="data2">Access our funds database, and start searching for funds matching your criteria</div>
                 </div>
-    
+
                 <div className="col-12-sm" id="funding"><div className="funding1">Design Collections</div>
                 <div className="funding2">Get help writing your applications, and make sure you arent missing anything</div>
                 </div>
-                
+
                 <div className="col-12-sm" id="manufacturers" > <div className="manufacturers1">Manufacturers</div>
                 <div className="manufacturers2">Get help writing your applications, and make sure you arent missing anything</div>
                 </div>
     </div>
 
 </div>
-  
+
 <div className="container">
 
-    
+
     <div className="row">
-        
+
         <div className="rolig col-6">
         Europe
         </div>
-        
+
         <div className="col-6">
         <div className="languagepicker">
-        
+
         <select className="selectpicker" data-width="fit">
         <option data-content='<span class="flag-icon flag-icon-us"> </span> English'>English</option>
         <option  data-content='<span class="flag-icon flag-icon-mx"></span> Español'>Español</option>
         </select>
-        
+
         </div>
         </div>
-    
-    </div> 
+
+    </div>
 
     <div className="row">
-    
+
         <div className="copyright col-12" id="copyright">
         @Copyright HippoPrint EUvsVirus
     </div>
-    
+
     </div>
-        
-    </div>        
-  </div> 
+
+    </div>
+  </div>
   </>
-  
+
   );
 }
 
