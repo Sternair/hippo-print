ebdb49eb9cc2c6a4fb59be4cc624cf45b4623999
Pedro Mosquera <pedro.mosquera.t@gmail.com>
2020-04-26 13:49:50 +0200

    cleaning imports

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import InnerNav from '../InnerNav/InnerNav'
 import SignUpButton from '../SignUp/SignUpButton'
 import './Main.scss'
 import { Auth } from 'aws-amplify'
