b9c31cbb5c7da34e07af5c1f6bf5f5ed7cb2fc49
Pedro Alejandro Mosquera <alejandro.mosquera@dynatrace.com>
2020-04-26 16:02:48 +0200

    Adding tabs on communiy page

--- components/ViewTabs/ViewTabs.tsx
+++ components/ViewTabs/ViewTabs.tsx
@@ -1,24 +1,44 @@
 import React, { FC } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import './ViewTabs.scss';
 
 const ViewTabs: FC = () => {
+  const router = useRouter();
+
   return (
     <ul className="view-tabs nav nav-tabs">
-      <li className="nav-item">
-        <a className="nav-link active" href="#">My Designs</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Community Projects</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Starred Items</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">View History</a>
-      </li>
+      {tabItems.map((item, i) => (
+        <li className="nav-item" key={i}>
+          <Link href={item.link}>
+            <a className={`nav-link${router.pathname === item.link ? ' active' : ''}`}>
+              {item.label}
+            </a>
+          </Link>
+        </li>
+      ))}
     </ul>
   );
 }
+
+const tabItems = [
+  {
+    label: 'My Designs',
+    link: '#',
+  },
+  {
+    label: 'Community Projects',
+    link: '/community',
+  },
+  {
+    label: 'Starred Items',
+    link: '#',
+  },
+  {
+    label: 'View History',
+    link: '#',
+  }
+]
 
 export default ViewTabs
